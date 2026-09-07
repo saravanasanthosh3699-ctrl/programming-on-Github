66b988e0c0dc7ecc5ebecc16a401195ecfd0be26
Santhosh GitHub/010620/ <saravanasanthosh3699@gmail.com>
2026-09-07 08:14:35 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,139 +1,20 @@
 
 
       
-       let numbers = [];
-let operators = [];
-let currentNumber = "";
-
-
-function press(value) {
-
-    // If the value is a number or decimal point
-    if ((value >= "0" && value <= "9") || value === ".") {
-
-        currentNumber += value;
-
-        document.getElementById("display").value = currentNumber;
-    }
-
-    // If the value is an operator
-    else {
-
-        if (currentNumber !== "") {
-
-            numbers.push(Number(currentNumber));
-            currentNumber = "";
-        }
-
-        operators.push(value);
-
-        document.getElementById("display").value += value;
-    }
+       function clearDisplay() {
+    document.getElementById("display").value = "";
 }
 
 
 function calculate() {
+    let expression = document.getElementById("display").value;
 
-    // Store the last number
-    if (currentNumber !== "") {
+    try {
+        let result = eval(expression);
 
-        numbers.push(Number(currentNumber));
+        document.getElementById("display").value = result;
     }
-
-    if (numbers.length === 0) {
-        return;
+    catch {
+        document.getElementById("display").value = "Error";
     }
-
-
-    // First calculate *, / and %
-    for (let i = 0; i < operators.length; i++) {
-
-        if (operators[i] === "*" ||
-            operators[i] === "/" ||
-            operators[i] === "%") {
-
-            let result;
-
-            if (operators[i] === "*") {
-                result = numbers[i] * numbers[i + 1];
-            }
-
-            else if (operators[i] === "/") {
-                result = numbers[i] / numbers[i + 1];
-            }
-
-            else if (operators[i] === "%") {
-                result = numbers[i] % numbers[i + 1];
-            }
-
-            numbers[i] = result;
-
-            numbers.splice(i + 1, 1);
-            operators.splice(i, 1);
-
-            i--;
-        }
-    }
-
-
-    // Then calculate + and -
-    let result = numbers[0];
-
-    for (let i = 0; i < operators.length; i++) {
-
-        if (operators[i] === "+") {
-
-            result = result + numbers[i + 1];
-        }
-
-        else if (operators[i] === "-") {
-
-            result = result - numbers[i + 1];
-        }
-    }
-
-
-    document.getElementById("display").value = result;
-
-
-    // Store result for next calculation
-    numbers = [];
-    operators = [];
-    currentNumber = result.toString();
 }
-
-
-function clearDisplay() {
-
-    numbers = [];
-    operators = [];
-    currentNumber = "";
-
-    document.getElementById("display").value = "";
-}
-        if (c == '*' || c == '/' || c == '%')
-        {
-        consol.log(g);
-        }
-
-         document.getElementById("display").value = g;
-             
-             numbers = [];
-             operators = [];
-             currentNumber = g.toString();
-        
-        function clearDisplay(){
-            numbers = [];
-            operators = [];
-            currentNumbers = "";
-
-            document.getElementById("display").value = "";
-        }
-        
-       
-       
-        
-        
-            
-
-    
